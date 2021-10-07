78eef267d4bee3be3a827ae2e923cea74b4154b4
edevregille <manudevregille@gmail.com>
2021-10-07 13:59:20 -0700

    enable multiple infra tags keys

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ require('dotenv').config();
 const {fetchEntities, fetchInfraAWSTag, addEntitiesTag, deleteEntitiesTag} = require('./utils');
 
 //------------------------------
-const TAG_KEY = "aws.awsRegion";
+const TAG_KEY = ["aws.awsRegion", "awsRegion"]; // same tag can have multiple keys
 const TAG_KEY_TO_ADD = "server_region";
 const NR_ACCOUNT_ID = 01234;
 //------------------------------
 
 const runToAddTags = async() => {
-    let entities = await fetchEntities(action = "addTags", NR_ACCOUNT_ID,TAG_KEY_TO_ADD);
+    let entities = await fetchEntities(action = "addTags", NR_ACCOUNT_ID);
     entities = await fetchInfraAWSTag(entities, TAG_KEY);
-    await addEntitiesTag(entities, TAG_KEY, TAG_KEY_TO_ADD);
+    await addEntitiesTag(entities, TAG_KEY_TO_ADD);
 }
 
 const runToDeleteTags = async() => {
--- utils.js
+++ utils.js
@@ -1,7 +1,7 @@
 const axios = require('axios');
 const fs = require('fs');
 
-const {FETCH_ENTITIES_QUERY, FETCH_HOSTS_TAGS, ADD_TAG_TO_ENTITY_QUERY, DELETE_TAG_TO_ENTITY_QUERY } = require('./queries');
+const { FETCH_ENTITIES_QUERY, FETCH_HOSTS_TAGS, ADD_TAG_TO_ENTITY_QUERY, DELETE_TAG_TO_ENTITY_QUERY } = require('./queries');
 const API_KEY = process.env.NEWRELIC_USER_API_KEY;
 
 const OPTIONS = {
@@ -60,7 +60,7 @@ exports.fetchEntities = async (action, accountId, TAG_KEY_TO_ADD) => {
     return allEntities;
 }
 
-exports.fetchInfraAWSTag = async (entities, tag_key) => {
+exports.fetchInfraAWSTag = async (entities, TAG_KEY) => {
     let promises = [];
     try {
         log(`${SYMBOLS.working} Fetching Hosts tags for each service...`);
@@ -79,42 +79,42 @@ exports.fetchInfraAWSTag = async (entities, tag_key) => {
         results.forEach((result, idx) => {
             if(result.data.errors){
                 log(`${SYMBOLS.error} Failed fetching tags for service ${entities[idx].guid} ${JSON.stringify(result.data.errors)}`, 'error');
-                entities[idx][tag_key] = null;
+                entities[idx]["tag"] = null;
             }
             else{
                 if(result.data.data.actor.entity && result.data.data.actor.entity.relatedEntities && result.data.data.actor.entity.relatedEntities.results && result.data.data.actor.entity.relatedEntities.results.length>0){
                     countInfraPresent ++;
                     let tags = result.data.data.actor.entity.relatedEntities.results.map(el => { return el.source.entity.tags }).flat();
                     if(tags.length > 0) {
-                        tags = tags.filter(el => el.key == tag_key).map(el => { return el.values });
+                        tags = tags.filter(el => TAG_KEY.indexOf(el.key) > -1).map(el => { return el.values });
                         if(tags && tags.length >0){
                             countAwsRegionTagPresent++;
-                            entities[idx][tag_key] = [...new Set(tags.flat())];
-                        } else entities[idx][tag_key] = null
+                            entities[idx]["tag"] = [...new Set(tags.flat())];
+                        } else entities[idx]["tag"] = null
                     }
                 }
-                else entities[idx][tag_key] = null;
+                else entities[idx]["tag"] = null;
             }
         });
-        log(`${SYMBOLS.success} Success fetching tags: ${countInfraPresent} service with Infra Hosts and ${countAwsRegionTagPresent} services with ${tag_key} tag`);
+        fs.writeFileSync('entities_fetched.json', JSON.stringify(entities));
+        log(`${SYMBOLS.success} Success fetching tags: ${countInfraPresent} service with Infra Hosts and ${countAwsRegionTagPresent} services`);
         return entities;
-       
     }catch(error){
         log(`${SYMBOLS.error} Failed (exception) infra AWS tags ${JSON.stringify(error)}`, 'error');
         return null;
     }
 }
 
-exports.addEntitiesTag = async (entities, TAG_KEY, TAG_KEY_TO_ADD ) => {
+exports.addEntitiesTag = async (entities, TAG_KEY_TO_ADD ) => {
     let promises = [];
-    entities = entities.filter(el => el[TAG_KEY] != null && el[TAG_KEY].length >0);
+    entities = entities.filter(el => el["tag"] != null && el["tag"].length >0);
     try{
         log(`${SYMBOLS.working} adding tag ${TAG_KEY_TO_ADD} to ${entities.length} entities...`);    
         entities.forEach(entity => {
             promises.push(
                 axios({
                     ...OPTIONS,
-                    data: JSON.stringify({"query": ADD_TAG_TO_ENTITY_QUERY(entity.guid, entity[TAG_KEY], TAG_KEY_TO_ADD)})
+                    data: JSON.stringify({"query": ADD_TAG_TO_ENTITY_QUERY(entity.guid, entity["tag"], TAG_KEY_TO_ADD)})
                 })
             )
         });
